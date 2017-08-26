b6539288485dff9a81e4ca37f3e1a66b83487d28
Misheq <mihaelz@live.com>
2017-08-26 17:03:06 +0200

    database now works

---
+++ src/com/youtube/rest/databasestatus/TestDatabase.java
@@ -0,0 +1,14 @@
+package com.youtube.rest.databasestatus;
+
+public class TestDatabase {
+
+	public static void main(String[] args) {
+	    Database db = new Database();
+	    try {
+	        db.connect();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    db.close();
+	}
+}
